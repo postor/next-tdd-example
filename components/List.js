ddc47bb78d434f30bf25f3f862ec658289a4802a
josh <postor@gmail.com>
2019-03-22 10:12:47 +0800

    basic

--- components/List.js
+++ components/List.js
@@ -4,19 +4,30 @@ import Item from './Item'
 
 export default ({ keyword }) => {
   const [list, setList] = useState([])
+  const [loading, setLoading] = useState(0)
   useEffect(() => {
+    if (!keyword) {
+      setList([])
+      return
+    }
+    setLoading(1)
     axios.get(`https://api.github.com/search/repositories`, {
       params: {
         q: keyword
       }
     }).then(res => {
+      setLoading(0)
       setList(res.data.items)
     }).catch(e => {
+      setLoading(0)
       alert(e.toString())
       console.log(e)
     })
   }, [keyword]);
 
+  if(loading>0){
+    return (<div data-testid="loading">loading...</div>)
+  }
   if (!list.length) {
     return (<div data-testid="no-result">no result found for <em>{keyword}</em></div>)
   }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useState } from 'react'
 import List from '../components/List'
 
-export default ()=>{
-  const keyword = 'next tdd example'
+export default () => {
+  const [keyword, setKeyword] = useState('next-tdd-example')
+  const [input, setInput] = useState('next-tdd-example')
   return (<div>
-    <List keyword={keyword}/>
+    <div>
+      <label>关键字</label>
+      <input value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        onClick={() => setKeyword(input)}
+      >搜索</button>
+    </div>
+    <List keyword={keyword} />
   </div>)
 }
